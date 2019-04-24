71cfd19c3707892919bd484eb0235719b4596ae2
andre31 <andre.rosario31@gmail.com>
2019-04-24 15:37:58 -0300

    adição e campanha xhtml e bean, alteração no dao

--- src/main/java/br/org/centrocac/dao/CampanhaDAO.java
+++ src/main/java/br/org/centrocac/dao/CampanhaDAO.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.org.centrocac.dao;
 
 import br.org.centrocac.entidade.Campanha;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 
 /**
@@ -19,19 +16,19 @@ public class CampanhaDAO extends GenericDAO<Campanha> {
 
     /**
      * Retorna as campanhas cuja data de encerramento estão no limite de datas
-     * informadas (entre início e fim). Apenas campanhas habilitadas serão 
+     * informadas (entre início e fim). Apenas campanhas habilitadas serão
      * retornadas.
      *
      * @param inicio
      * @param fim
      * @return Lista de campanhas
      */
-    public List<Campanha> obter(Date inicio, Date fim) {
+    public List<Campanha> obter(Date inicio, Date fim, Boolean habilitada) {
         EntityManager em = getEntityManager();
         String sql = "select c from Campanha c "
                 + "where "
-                + ":inicio <= c.dataFim and "
-                + ":fim <= c.dataFim and "
+                + "c.dataFim >= :inicio and "
+                + "c.dataFim <= :fim and "
                 + "c.habilitada = :habilitada ";
         Query query = em.createQuery(sql);
         List<Campanha> resposta = null;
@@ -39,7 +36,7 @@ public class CampanhaDAO extends GenericDAO<Campanha> {
             resposta = (List<Campanha>) query.
                     setParameter("inicio", inicio).
                     setParameter("fim", fim).
-                    setParameter("habilitada", Boolean.TRUE).
+                    setParameter("habilitada", habilitada).
                     getResultList();
         } catch (Exception e) {
             e.printStackTrace(System.err);
@@ -48,4 +45,38 @@ public class CampanhaDAO extends GenericDAO<Campanha> {
         }
         return resposta;
     }
+
+    public List<Campanha> obterTodos(Boolean habilitada) {
+        EntityManager em = getEntityManager();
+        Query query = em.createQuery("select c from Campanha c where c.habilitada = :habilitada").
+                setParameter("habilitada", habilitada);
+        List<Campanha> resposta = null;
+        try {
+            resposta = query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        } finally {
+            em.close();
+        }
+        return resposta;
+    }
+
+    public void atualizar(Campanha c) {
+        EntityManager em = getEntityManager();
+        EntityTransaction t = em.getTransaction();
+        try {
+            t.begin();
+            em.merge(c);
+            t.commit();
+        } catch (Exception e) {
+            System.out.println("error atulizar campanha");
+            e.printStackTrace(System.err);
+            if (t != null && t.isActive()) {
+                t.rollback();
+            }
+        } finally {
+            em.close();
+        }
+    }
+
 }
